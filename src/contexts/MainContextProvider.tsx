6c91e41624e97dbd3e73e4947ad3d926ffd79a44
nacmonad <macnomad84@gmail.com>
2024-01-04 18:37:47 -0500

    variation on sw init for push

--- src/contexts/MainContextProvider.tsx
+++ src/contexts/MainContextProvider.tsx
@@ -78,29 +78,33 @@ export const MainContextProvider: React.FC<MainContextProviderProps> = ({ childr
                       headers.append('Content-Type', 'application/json');
                       headers.append('x-odoo-partner-id', String(partner.id));
 
-                    fetch(`${process.env.NEXT_PUBLIC_PUSH_HOST}/api/subscribe`, {
-                      method:"POST",
-                      headers,
-                      body: JSON.stringify(subscription)
-                    })
-                    .then(res=> res.json())
-                    .then(sub=> {
-                      console.log("Subscribed!", sub);
-                      if(sub.subscription) {
-                        const subCopy = { ...sub.subscription };
-                        //don't need to expose private info but its' there
-                        delete subCopy.keys;
-                        setSubscription(subCopy);
-                      }
-                    }).catch((err : Error | null | undefined) => console.error(err))}
+                      fetch(`${process.env.NEXT_PUBLIC_PUSH_HOST}/api/subscribe`, {
+                        method: "POST",
+                        headers,
+                        body: JSON.stringify(subscription)
+                      })
+                        .then(res => res.json())
+                        .then(sub => {
+                          console.log("Subscribed!", sub);
+                          if (sub.subscription) {
+                            const subCopy = { ...sub.subscription };
+                            // Don't need to expose private info but it's there
+                            delete subCopy.keys;
+                            setSubscription(subCopy);
+                          }
+                        })
+                        .catch((err) => console.error('Error during subscription request:', err));
+                  }
                     
                   })
                   .catch(error => {
                     console.error('Error subscribing to push notifications:', error);
                   });
               });
+
+
         }
-        });
+      });
     }
    }
    function initQrReceiveCode() {
